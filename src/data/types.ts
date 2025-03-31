fd283493269885c12083cb8bb194021c5b80af1f
Bannon Tanner <bannon.n.tanner@gmail.com>
2025-03-30 21:26:08 -0500

    comments on type

--- src/data/types.ts
+++ src/data/types.ts
@@ -1,7 +1,22 @@
 export type TreeNode = {
+  /**
+   * Unique identifier for the node.
+   */
   id: string
+  /**
+   * Name of the node, typically the component name.
+   */
   name: string
+  /**
+   * Optional properties associated with the node.
+   */
   props?: Record<string, any>
+  /**
+   * Optional state associated with the node.
+   */
   state?: Record<string, any>
+  /**
+   * Optional event handlers associated with the node.
+   */
   children?: TreeNode[]
 }
